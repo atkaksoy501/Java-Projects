99421efd749d1bd48c26cbb49ceec129679606c1
Atakan Aksoy <72624958+atkaksoy501@users.noreply.github.com>
2021-08-22 14:24:40 +0300

    Calculator v0.5's class file

---
+++ .idea/DortIslem.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class DortIslem {
+    public int Topla (int s1, int s2) {
+        return s1 + s2;
+    }
+
+    public int Cikar (int s1, int s2) {
+        return s1 - s2;
+    }
+
+    public int Carp (int s1, int s2) {
+        return s1 * s2;
+    }
+
+    public int Bol (int s1, int s2) {
+        return s1 / s2;
+    }
+}
